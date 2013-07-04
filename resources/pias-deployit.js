157397171658240548039e4b253f7651f8bbe7d9
Ryan Kelly <ryan@rfk.id.au>
2013-07-04 23:22:44 +1000

    More on the talk

---
+++ resources/pias-deployit.js
@@ -0,0 +1,306 @@
+var pias_deployit_data = {
+  "events": [
+    {
+      "act": "OPEN", 
+      "size": [
+        60, 
+        16
+      ], 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.493912935256958
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "rfk@durian:~$ ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.0495600700378418
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "rfk@durian:~$ ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "python", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.017245054244995117
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "Python 2.7.4 (default, Apr 19 2013, 18:28:01) \r\n[GCC 4.7.3] on linux2\r\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information.\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.0013020038604736328
+    }, 
+    {
+      "act": "WRITE", 
+      "data": ">>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "import requests", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.15917205810546875
+    }, 
+    {
+      "act": "WRITE", 
+      "data": ">>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "s = requests.session()", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.0037298202514648438
+    }, 
+    {
+      "act": "WRITE", 
+      "data": ">>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "s.auth = (\"rfkelly\", \"SunshineAndPuppies\")", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n>>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n>>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "s.get(\"http://pyconau.services.mozilla.com/1.1/rfkelly/", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "i", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "i \r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "nfo/collections\")", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.14397096633911133
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "<Response [200]>\r\n>>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u001b", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "[", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "A", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "s.get(\"http://pyconau.services.mozilla.com/1.1/rfkelly/in\rnfo/collections\")", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": "/", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u007f", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\b\u001b[K", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "ECHO", 
+      "data": ".content", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.08800792694091797
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "'{\"tabs\": 1372852222.63, \"clients\": 1372852221.44, \"crypto\": 1372852220.96, \"meta\": 1372852222.83, \"bookmarks\": 1372852221.73, \"prefs\": 1372852222.43, \"history\": 1372852222.04}'\r\n>>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n>>> ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u0004", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.054611921310424805
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "rfk@durian:~$ ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\r", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "PAUSE", 
+      "duration": 0.044271230697631836
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "rfk@durian:~$ ", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "READ", 
+      "data": "\u0004", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "WRITE", 
+      "data": "exit\r\n", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }, 
+    {
+      "act": "CLOSE", 
+      "term": "db7dd5d96db24eaeb0033a735e4d011e"
+    }
+  ]
+}
